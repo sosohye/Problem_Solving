55c737279ef4c9a9d791ca7e570fb32e7c523f26
sosohye <pjhpoqo@gmail.com>
2023-03-09 17:30:33 +0900

    c++ template

---
+++ Baekjoon/template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+void input()
+{
+    cin >> n;
+}
+
+void solve()
+{
+
+}
+
+void output()
+{
+    cout << n << endl;
+}
+
+int main()
+{
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    input();
+    solve();
+    output();
+
+    return 0;
+}
